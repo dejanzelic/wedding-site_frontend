02abf8f73788f9f0572b5e38d0c4b6be73dab4aa
Dejan <dejanzelic@gmail.com>
2021-01-24 20:11:00 -0700

    Added Navbar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,29 @@
+<template>
+<div >
+    <div class="container">
+      <b-navbar toggleable="lg" type="light">
+        <b-navbar-brand href="#"
+          class="font-weight-bold navbar-brand-centered">
+          zelic
+        </b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+          <b-navbar-nav>
+            <b-nav-item href="#">Home</b-nav-item>
+            <b-nav-item href="#" disabled>Disabled</b-nav-item>
+          </b-navbar-nav>
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item href="#">Home</b-nav-item>
+            <b-nav-item href="#" disabled>Disabled</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar',
+};
+</script>
