27a40e0939d55aae70a681e068757d0916ddfadc
Dejan Zelic <d.zelic@offensive-security.com>
2021-04-26 18:13:30 -0700

    Fix links

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,16 +2,17 @@
   <div class="nav-centered">
     <div class="container">
       <b-navbar toggleable="lg" type="light">
-        <b-navbar-brand href="#" class="font-weight-bold navbar-brand-centered">
+        <b-navbar-brand class="font-weight-bold navbar-brand-centered">
           zelic
         </b-navbar-brand>
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav>
-            <b-nav-item href="#">Home</b-nav-item>
+            <b-nav-item :to="{ name: 'Home'}">Home</b-nav-item>
+            <b-nav-item :to="{ name: 'OurStory'}">Our Story</b-nav-item>
           </b-navbar-nav>
           <b-navbar-nav class="ml-auto">
-            <b-nav-item href="#">Home</b-nav-item>
+            <b-nav-item :to="{ name: 'Info'}">Info</b-nav-item>
             <b-nav-item-dropdown text="🌐 Lang" right v-model="$i18n.locale">
               <b-dropdown-item
                 v-for="(lang, i) in langs"
