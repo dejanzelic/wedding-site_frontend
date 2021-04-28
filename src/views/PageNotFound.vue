fe2a69654ddd633957e4b0fb646c12fcf8c35cce
Dejan Zelic <d.zelic@offensive-security.com>
2021-04-27 17:58:18 -0700

    Added 404

---
+++ src/views/PageNotFound.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <section class="intro" style="background-size: cover">
+      <b-container>
+        <b-row class="d-flex justify-content-center align-items-center">
+          <b-col col md="12" class="text-center">
+            <h1 id="hero-text">
+              404 - Not Found
+            </h1>
+            <h3>But here are some pictures of us:</h3>
+          </b-col>
+        </b-row>
+      </b-container>
+    </section>
+    <Gallery/>
+  </div>
+</template>
+
+<script>
+import Gallery from '../components/Gallery.vue'
+
+export default {
+  name: "PageNotFound",
+  components: {
+    Gallery
+  },
+};
+</script>
+
+<style scoped>
+  #hero-text {
+      font-size: calc(4.575rem + 3.9vw);
+      font-family: 'Antic Didone', serif;
+  }
+  #hero-and {
+    font-size: calc(1.575rem + 3.9vw);
+    font-style: italic;
+  }
+  .intro{
+    padding-top: 100px;
+    padding-bottom: 80px;
+  }
+</style>
